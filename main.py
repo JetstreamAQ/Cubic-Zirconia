2b3a3df5dccbb10b022f2c4e91f9ce0dc009f2c8
JetstreamAQ <45304769+JetstreamAQ@users.noreply.github.com>
2025-08-17 21:27:34 +0000

    Fixed indentation: replaced tabs for spaces

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import logging
 import asyncio
 
 from dotenv import load_dotenv
+from discord import app_commands
 from discord.ext import commands
-#from discord_slash import SlashCommand
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,8 +13,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = commands.Bot(command_prefix='+', owner_ids=os.getenv('OWNER_ID'), intents=intents)
-#slash = SlashCommand(client, sync_commands=True)
+bot = commands.Bot(command_prefix='+', owner_ids=os.getenv('OWNER_ID'), intents=intents)
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -22,9 +21,9 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-@client.event
+@bot.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the showroom. | prefix: +"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the showroom. | prefix: +"))
     print(f'[INFO] User >> {client.user}\n[INFO] Sign In >> Successful')
 
 #####
@@ -33,17 +32,16 @@ async def on_ready():
 
 async def load():
     for filename in os.listdir('./cogs'):
-       	if filename.endswith('.py'):
-           	await client.load_extension(f'cogs.{filename[:-3]}')
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
 #####
 # COGS END
 #####
 
 async def main():
-	async with client:
-		await load()
-		await client.start(TOKEN, reconnect=True)
+    async with bot:
+        await load()
+        await bot.start(TOKEN, reconnect=True)
 
-#client.run(TOKEN, reconnect=True)
 asyncio.run(main())
